1e8f8b01fd5b00cd057fec418f22392cc5f2f988
Hongwang Lv <Hongwang Lv>
2017-03-20 21:39:42 +0800

    add

--- public/index.php
+++ public/index.php
@@ -12,6 +12,16 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
-// 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+// define('APP_PATH', __DIR__ . '/../application/');
+// // 加载框架引导文件
+// require __DIR__ . '/../thinkphp/start.php';
+echo 'hello';
+$str1="hello php ";
+//字符串索引
+echo strpos('haystack', 'a');echo "<br>";
+//字符串截取
+echo substr('haystack',1);echo "<br>";
+echo substr('haystack',1,2);echo "<br>";
+//字符串转数组
+$result=str_split($str1);
+print_r($result);echo "<br>";
